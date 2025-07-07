870406d1fe9a31ededf9446e6ab674eff88ae3ca
AboodWadie <aboodewadiea@gmail.com>
2025-07-07 15:17:11 +0300

    js for new task + added new rask button

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -11,3 +11,29 @@ window.addEventListener('DOMContentLoaded', () => {
     document.documentElement.classList.remove('darkmode');
   }
 });
+
+let addNewTaskHTML = '' ;
+const taskInput = document.querySelector('.mr-2');
+const btn = document.querySelector('.btn-outline-info');
+const taskList = document.querySelector('.item');
+
+
+
+function addTask(){
+  const task = taskInput.value.trim() ;
+   if (task !== '') {
+      const li = document.createElement('li') ;
+      li.className =  'list-group-item';  
+      li.textContent = task; 
+    taskList.appendChild(li);  
+    taskInput.value = '';    
+    console.log(alert('task added sussesfuly')) ;
+  }
+ }
+
+ btn.addEventListener('click', addTask);
+taskInput.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    addTask();
+  }
+});
